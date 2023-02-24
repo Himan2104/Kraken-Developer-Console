a756790dc66f4f55304f39be3faed34fef5fd03f
Himanshu Parchand <himan2104@gmail.com>
2023-02-24 22:33:52 +0530

    Refactor asmdefs

--- Runtime/DeveloperConsoleUI.cs
+++ Runtime/DeveloperConsoleUI.cs
@@ -4,7 +4,7 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-[assembly : InternalsVisibleTo("kraken.developer-console.Editor")]
+[assembly : InternalsVisibleTo("Kraken.DeveloperConsole.Editor")]
 
 namespace Kraken.DevCon
 {    
